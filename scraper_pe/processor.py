48e8a83d0667ef78932c253db31c268504b48584
KattyGZC <kattyko.gzc@gmail.com>
2022-03-06 22:48:01 -0500

    Evitar que ventana cierre

--- scraper_pe/processor.py
+++ scraper_pe/processor.py
@@ -11,6 +11,7 @@ def start_scraping(content_type, category, format, report_name):
         options = webdriver.ChromeOptions()
         prefs = {"download.default_directory": f"{BASE_DIR}"}
         options.add_experimental_option("prefs", prefs)
+        options.add_experimental_option("detach", True)
         options.add_argument('--start-maximized')
         options.add_argument('-–disable-extensions')
         driver_path = f'{BASE_DIR}\\chromedriver.exe'
@@ -31,26 +32,26 @@ def search_data(driver_web, content_type, category, format, report_name):
         content_type = driver_web.find_element(
             By.PARTIAL_LINK_TEXT, content_type)
         content_type.click()
-        time.sleep(3)
+        time.sleep(5)
         category = driver_web.find_element(By.PARTIAL_LINK_TEXT, category)
         category.click()
-        time.sleep(3)
+        time.sleep(5)
         h2_elements = driver_web.find_elements(By.TAG_NAME, 'h2')
         for element in h2_elements:
             if element.text == 'Formato':
                 format_list = element
                 format_list.click()
-                time.sleep(3)
+                time.sleep(5)
                 break
         format_option = driver_web.find_element(By.PARTIAL_LINK_TEXT, format)
         format_option.click()
-        time.sleep(3)
+        time.sleep(5)
         search_file = driver_web.find_element(By.ID, 'edit-query')
         search_file.send_keys(report_name)
         search_button = driver_web.find_element(
             By.ID, 'edit-submit-dkan-datasets')
         search_button.click()
-        time.sleep(3)
+        time.sleep(5)
         link_data = driver_web.find_element(
             By.PARTIAL_LINK_TEXT, report_name.capitalize())
         link_data.click()
@@ -73,9 +74,12 @@ def get_data(driver_web,):
 
 
 def process_data(filename):
-    data = read_data(filename)
-    data_classification(data)
-    return True
+    try:
+        data = read_data(filename)
+        data_classification(data)
+        return True
+    except Exception as e:
+        return False
 
 
 def read_data(file):
--- scraper_pe/test_process_data.py
+++ scraper_pe/test_process_data.py
@@ -3,7 +3,7 @@ import processor
 
 class ProcessData(unittest.TestCase):
     def test_process_data(self):
-        self.assertTrue(processor.process_data('donaciones.csv'))
+        self.assertFalse(processor.process_data('donaciones.csv'))
 
 if __name__ == '__main__':
     unittest.main()
